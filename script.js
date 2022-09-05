391c2a83d6682d8da337e5bf3c1c0039536c7f89
solixbek03 <clashbommm@gmail.com>
2022-09-05 09:41:05 +0500

    add commit

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 
 const elInput = document.querySelector('.input')
-const elButton = document.querySelector('.button')
+const elButton = document.querySelector('.container')
 const elResult = document.querySelector('.result')
 
 let bozorlikRoyhati = [];
 // localStorage.clear()
 
 
-elButton.addEventListener('click', function(evt){
+elButton.addEventListener('change', function(evt){
   let elinputVall = elInput.value;
   elInput.value = '';
   bozorlikRoyhati.push(elinputVall);
   localStorage.setItem('bozorlik', bozorlikRoyhati);
-  let boom = localStorage.getItem('bozorlik');
 
+  let boom = localStorage.getItem('bozorlik');
 
   elResult.innerHTML = '';
   let li = document.createElement("li")
   li.textContent = boom
   li.classList.add('text-light')
   elResult.appendChild(li)
-  
 })
 
+let boom = localStorage.getItem('bozorlik');
+bozorlikRoyhati.push(boom)
 
+elResult.innerHTML = '';
+let li = document.createElement("li")
+li.textContent = boom
+li.classList.add('text-light')
+elResult.appendChild(li)
+
+
+  // elResult.innerHTML = '';
+  // let li = document.createElement("li")
+  // li.textContent = boom
+  // li.classList.add('text-light')
+  // elResult.appendChild(li)
